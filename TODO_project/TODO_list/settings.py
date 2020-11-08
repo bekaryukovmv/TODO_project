6d122205aa7a197a2262472391cc6a8fe46c377e
Maxim Bekaryukov <bekaryukovmv@gmail.com>
2020-11-08 23:01:22 +0700

    fix docker config

--- TODO_project/TODO_list/settings.py
+++ TODO_project/TODO_list/settings.py
@@ -132,9 +132,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "TODO_list/static/"),
-]
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
